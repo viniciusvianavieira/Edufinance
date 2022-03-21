280cdc3656e27d49b45f5a8a7fe696b51b2f4b87
Vinicius Viana <vieiravianavinicius@gmail.com>
2022-03-21 08:33:53 -0300

    main

--- ApiYoutube/estatisticas_youtube.py
+++ ApiYoutube/estatisticas_youtube.py
@@ -1,5 +1,6 @@
 from os import PRIO_USER
 import statistics
+from traceback import print_tb
 import requests
 import json
 
@@ -11,21 +12,58 @@ class estatisticasYoutube:
         self.key_api = key_api
         self.id_canal = id_canal
         self.estatisticas_canal = None
+        self.estatisticas_video = None
+        
 
     def pegar_estatisticas_youtube(self):
-        url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.id_canal}&key={self.key_api}'#O f''(fstring) foi utilizado para incluir variaveis {self.id_canal} no meio da string
+        url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.id_canal}&key={self.key_api}"#O f''(fstring) foi utilizado para incluir variaveis {self.id_canal} no meio da string
 
         json_url = requests.get(url) #pega o formato json do url que estava na web
-        data = json.loads(json_url.text) #pega o formato json do url e transforma em texto
+        dados = json.loads(json_url.text) #pega o formato json do url e transforma em texto no foramto de PYTHON
 
-        try:
-            data = data["items"][0]['statistics'] #filtrando para pegar somente a estatistica
+        try: #VAI TENTAR RODAR O CÓDIGO SE FALHAR VAI PRO EXCEPT
+            dados = dados["items"][0]['statistics'] #filtrando para pegar somente a estatistica. O [0] É A POSIÇÃO DO ITEMS(PRIMEIRO)
 
-        except:
-            data = None
+        except: #RODA O CÓDIGO CASO A PRIMEIRA PARTE ESTEJA ERRADA
+            dados = None
+
+        self.estatisticas_canal = dados #armazazenando as estatisticas do canal
+
+        return dados
+
+    def pegar_estatisticas_video_canal(self):
+
+        videos_canal = self.pegar_videos_canal(limit=50) #só podemos pegar até 50 videos, limite do youtube
+
         
-        return data
 
 
 
-print("ola")
+    def pegar_videos_canal(self, limit=None):
+
+        url = f'//www.googleapis.com/youtube/v3/search?key={self.key_api}&channelId={self.id_canal}&part=id&order=date' #só retorna os 5 ultimos videos por isso...
+
+        if limit is not None and isinstance(limit, int):
+            url += '&maxResults=' + str(limit) #... precisamos extender esse limite
+        
+        print(url)
+        
+    
+    
+
+    def dump(self):
+        if self.estatisticas_canal is None:
+            return
+        
+        
+        nome_canal = "Brenno Sullivan" #pegar do youtube dados
+        nome_canal = nome_canal.replace(" ","_").lower() #substituir os espaços por '_' e deixar tudo minusculo
+        nome_arquivo = nome_canal + '.json' #criar o nome do arquivo no vscode formato json
+
+        with open(nome_arquivo,'w') as f: #abriindo o arquivo no "write mode" como fstring (formatação de texto)
+            json.dump(self.estatisticas_canal,f, indent=4) # jogando a estatisticas do canal no arquivo .json, formatando ela por f-string, e organizando ela mais "bonito"
+
+        print('Arquivo descarregado')
+
+    
+     
--- ApiYoutube/main.py
+++ ApiYoutube/main.py
@@ -1,12 +1,14 @@
 #https://www.youtube.com/watch?v=5qtC-tsQ-wE (Link do video)
 
+
 from estatisticas_youtube import estatisticasYoutube #importando a class: estatisticasyoutube do outro arquivo python
 
 key_api = 'AIzaSyDRtc_ubbFC_LnJ1vcBuWsXalYMb_HSGCs' #chave obtida a partir do google developers
 id_canal = 'UC7zXCDXJssKjkij2N2az39g' #id do canal obtida pelo youtube
 
 youtube = estatisticasYoutube(key_api, id_canal) #variavel que é atribuida a class criada, onde retiramos dela dois parametros
-data = youtube.pegar_estatisticas_youtube()
+#youtube.pegar_estatisticas_youtube()
 
-print(data)
+#youtube.dump()
 
+youtube.pegar_estatisticas_video_canal()
