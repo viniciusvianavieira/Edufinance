a1669c338d53a266bac81f6d13df85e679b6be65
Vinicius Viana <vieiravianavinicius@gmail.com>
2022-04-01 17:20:41 -0300

    main

--- ApiInstagram/ApiTesteInstagram.py
+++ ApiInstagram/ApiTesteInstagram.py
@@ -1,3 +1,5 @@
+
+
 # Import Libraries
 import requests
 import json
@@ -6,15 +8,15 @@ import pandas as pd
 
 # Define Parameters Dictionary
 params = dict()
-params['access_token'] = 'EAAr1Pr9o9QcBAPzITBpXfoXdxEyyHQZBVBtA7ZAZAnhjZAbJPevJuZBvEeZAHGF3sUoN0LpiDx1Q8RGueuRcXfV56ZATPnkoI4KwnUtFMeoFK8TNTOZAWbnGNwWfQHBVTQX0aZAcvRfFcESbrtJnq0proOaVmsYRr4ZCWU2JyDOq2w1CoqUZAEKZCYngTzMDmk9bc70ZD'        # TOKEN DE ACESSO
-params['client_id'] = '3084399615210759'                  # not an actual client id
-params['client_secret'] = '199bcd728b98322cec2dd66cfc9ffa5a'     # not an actual client secret
+params['access_token'] = 'EAAbdojcuBagBABml0d87C2v5HzgENjMpmtFkzCyu6eoH83YkOfoUnT2BDCovOydpimWRcRA1eLZARkfbbGm903xbJZCJsEbBc9pOHjdDcAgAr6ZB918jV1zihFgZCHimmTF6csfgZCF74OBofNbdNpkWUPpmXFHPparqf4EyQ9o9W6gsjsAXSEYxdYk6tI2PtbgLZAyArryvt1tEZCxY7Xm'        # TOKEN DE ACESSO
+params['client_id'] = '4895545683873070'                  # not an actual client id
+params['client_secret'] = '44918cdd45355b5c3499bff5a6f174d2'     # not an actual client secret
 params['graph_domain'] = 'https://graph.facebook.com'
 params['graph_version'] = 'v12.0'
 params['endpoint_base'] = params['graph_domain'] + '/' + params['graph_version'] + '/'
-params['page_id'] = '106152182044947'                  # not an actual page id
-params['instagram_account_id'] = '17841401411209088'        # not an actual instagram business account id
-params['ig_username'] = 'viniciusvianavieira'
+params['page_id'] = '527015930816521'                  # not an actual page id
+params['instagram_account_id'] = '17841406234741156'        # not an actual instagram business account id
+params['ig_username'] = 'edufinance'
 
 endpointParams = dict()
 endpointParams['input_token'] = params['access_token']
@@ -109,3 +111,104 @@ for insight in media_insight:
 # Create DataFrame
 df_media_insight = pd.DataFrame(list(zip(engagement_list, impressions_list, reach_list, saved_list)), columns =['Engagement', 'Impressions', 'Reach', 'Saved'])
 df_media_insight.head(24)
+
+
+-----------------------------------------------------------
+
+
+# Define URL
+url = params['endpoint_base'] + params['instagram_account_id'] + '/media'
+
+
+# Define Endpoint Parameters
+endpointParams = dict()
+endpointParams['fields'] = 'id,caption,media_product_type,media_type,permalink,timestamp,username,like_count,comments_count'
+endpointParams['access_token'] = params['access_token']
+
+# Requests Data
+data = requests.get(url, endpointParams )
+basic_insight = json.loads(data.content)
+print(basic_insight['data'])
+
+
+fotos_caption = []
+fotos_product_type = []
+fotos_type = []
+fotos_permalink = []
+fotos_timestamp = []
+fotos_username = []
+fotos_like_count = []
+fotos_comments_count = []
+
+fotos_ids = []
+for i in range(0,len(basic_insight['data'])):
+    fotos_ids.append(basic_insight['data'][i]['id'])
+    # fotos_caption.append(basic_insight['data'][i]['caption'])
+    # fotos_product_type.append(basic_insight['data'][i]['media_product_type'])
+    # fotos_type.append(basic_insight['data'][i]['media_type'])
+    # fotos_permalink.append(basic_insight['data'][i]['permalink'])
+    # fotos_timestamp.append(basic_insight['data'][i]['timestamp'])
+    # fotos_username.append(basic_insight['data'][i]['username'])
+    # fotos_like_count.append(basic_insight['data'][i]['like_count'])
+    # fotos_comments_count.append(basic_insight['data'][i]['comments_count'])
+
+
+Existe_proxima_pagina = True
+while Existe_proxima_pagina:
+    try:
+        url_next = basic_insight['paging']['next']
+        data_next = requests.get(url_next)
+        basic_insight = json.loads(data_next.content)
+        for i in range(0,len(basic_insight['data'])):
+            fotos_ids.append(basic_insight['data'][i]['id'])
+            # try:
+            #     fotos_caption.append(basic_insight['data'][i]['caption'])
+            # except:
+            #     fotos_caption.append('ERROR404')
+            # fotos_product_type.append(basic_insight['data'][i]['media_product_type'])
+            # fotos_type.append(basic_insight['data'][i]['media_type'])
+            # fotos_permalink.append(basic_insight['data'][i]['permalink'])
+            # fotos_timestamp.append(basic_insight['data'][i]['timestamp'])
+            # fotos_username.append(basic_insight['data'][i]['username'])
+            # fotos_like_count.append(basic_insight['data'][i]['like_count'])
+            # fotos_comments_count.append(basic_insight['data'][i]['comments_count'])
+            
+            
+    except Exception as e:
+        Existe_proxima_pagina = False
+        break 
+
+
+----------------------------------------------------
+
+
+# Define URL
+url = params['endpoint_base'] + params['instagram_account_id'] + '/media'
+
+
+# Define Endpoint Parameters
+endpointParams = dict()
+endpointParams['fields'] = 'id,caption,media_product_type,media_type,permalink,timestamp,username,like_count,comments_count'
+endpointParams['access_token'] = params['access_token']
+
+# Requests Data
+data = requests.get(url, endpointParams )
+basic_insight = json.loads(data.content)
+all_basics_insights = pd.DataFrame(basic_insight['data'])
+
+
+
+Existe_proxima_pagina = True
+while Existe_proxima_pagina:
+    try:
+        url_next = basic_insight['paging']['next']
+        data_next = requests.get(url_next)
+        basic_insight = json.loads(data_next.content)
+        
+        
+    except Exception as e:
+        Existe_proxima_pagina = False
+        break
+    dataframeaux = pd.DataFrame(basic_insight['data'])
+    
+dataframeaux.append(all_basics_insights)
